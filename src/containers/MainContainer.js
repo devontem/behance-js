2f4729c6dfffd6eca512a210a605ed7a24ff819e
Devonte MacGlashan <devontem@yahoo-inc.com>
2018-02-22 18:12:34 -0800

    added store and MainContainer

---
+++ src/containers/MainContainer.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { connect } from 'react-redux';
+
+import Main from "./../components/Main.js";
+
+const mapStateToProps = (state, ownProps) => {
+	return {
+		...state // spread state properties
+	}
+};
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+	return {
+		searchUsers: function(id){
+			dispatch({
+				type: 'SEARCH_USERS',
+				payload: axios.get('/users.json')
+			});
+		},
+		getOneUser: function(id){
+			dispatch({
+				type: 'GET_ONE_USER',
+				payload: axios.get('/user.json')
+			});
+		}
+	}
+};
+
+class MainContainer extends Component {
+	render(){
+		return <Main {...this.props} />
+	}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(MainContainer);
