ace1948143418179ab1d0dcab0298b8fc6a4a640
Devonte MacGlashan <devontem@yahoo-inc.com>
2018-02-23 17:49:41 -0800

    added styles, card formatting displaying bio, username, etc

--- src/components/UserPage.js
+++ src/components/UserPage.js
@@ -1,4 +1,11 @@
 import React, {Component} from 'react';
+import {Card, CardHeader, CardText} from 'material-ui/Card';
+
+const styles = {
+	bold: {
+		fontWeight: 'bold'
+	}
+};
 
 class UserPage extends Component {
 	searchUsers(){
@@ -7,10 +14,29 @@ class UserPage extends Component {
 	}
 
 	render(){
+		const { user } = this.props.user;
 		return (
-			<div>
-				<h1>User Page</h1>
-			</div>
+			<Card>
+				<CardHeader
+					title={`@${user.username}`} 
+					subtitle={`${user.display_name} - ${user.city}, ${user.country}`}
+					avatar={(user.images || {})[50]}
+				/>
+				<CardText>
+					<div>
+						<h3 style={styles.bold}>Occupation</h3>
+						<p>{user.occupation}</p>
+					</div>
+					<div>
+						<h3 style={styles.bold}>Fields</h3>
+						<p>{ (user.fields || []).join(', ') }</p>
+					</div>
+					<div>
+						<h3 style={styles.bold}>Where, When and What</h3>
+						<p>{(user.sections || {})['Where, When and What']}</p>
+					</div>
+				</CardText>
+			</Card>
 		);
 	}
 }
