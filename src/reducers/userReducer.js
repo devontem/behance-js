adbd9b05580e94a5e2bd11335fb4e343922f3029
Devonte MacGlashan <devontem@yahoo-inc.com>
2018-02-24 21:25:11 -0800

    updated userReduces to handle actions for getProjects, getExperience, getFollowers, & getFollowing

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -18,6 +18,46 @@ const user = (state = initalState, action) => {
 		case 'USER_SEARCH_ERROR':
 			return {...state, error: true, errorMessage: action.message, user: null}
 
+		case 'GET_USER_PROJECTS':
+			return state;
+		case 'GET_USER_PROJECTS_PENDING':
+			return {...state, pending: true, error: false};
+		case 'GET_USER_PROJECTS_FULFILLED':
+			return {...state, pending: false, error: false, projects: action.payload.data.projects};
+		case 'GET_USER_PROJECTS_REJECTED':
+			message = (action.payload.message) ? action.payload.message : 'An error has occured. Please try again later.';
+			return {...state, pending: false, error: true, errorMessage: message, projects: null };
+
+		case 'GET_USER_EXPERIENCE':
+			return state;
+		case 'GET_USER_EXPERIENCE_PENDING':
+			return {...state, pending: true, error: false};
+		case 'GET_USER_EXPERIENCE_FULFILLED':
+			return {...state, pending: false, error: false, experience: action.payload.data.work_experience};
+		case 'GET_USER_EXPERIENCE_REJECTED':
+			message = (action.payload.message) ? action.payload.message : 'An error has occured. Please try again later.';
+			return {...state, pending: false, error: true, errorMessage: message, experience: null};
+
+		case 'GET_USER_FOLLOWING':
+			return state;
+		case 'GET_USER_FOLLOWING_PENDING':
+			return {...state, pending: true, error: false};
+		case 'GET_USER_FOLLOWING_FULFILLED':
+			return {...state, pending: false, error: false, following: action.payload.data.following};
+		case 'GET_USER_FOLLOWING_REJECTED':
+			message = (action.payload.message) ? action.payload.message : 'An error has occured. Please try again later.';
+			return {...state, pending: false, error: true, errorMessage: message, following: null};
+
+		case 'GET_USER_FOLLOWERS':
+			return state;
+		case 'GET_USER_FOLLOWERS_PENDING':
+			return {...state, pending: true, error: false};
+		case 'GET_USER_FOLLOWERS_FULFILLED':
+			return {...state, pending: false, error: false, followers: action.payload.data.followers};
+		case 'GET_USER_FOLLOWERS_REJECTED':
+			message = (action.payload.message) ? action.payload.message : 'An error has occured. Please try again later.';
+			return {...state, pending: false, error: true, errorMessage: message, followers: null};
+
 		default:
 			return state;
 	}
