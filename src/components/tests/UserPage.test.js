cb1128d3580da84cc9e6d510349959fc121ac815
Devonte MacGlashan <devontem@yahoo-inc.com>
2018-02-25 13:03:26 -0800

    updated the user object, refactored how props are passed to shallow components, and added texts for existence of props inside the UserPage componenet

--- src/components/tests/UserPage.test.js
+++ src/components/tests/UserPage.test.js
@@ -28,30 +28,39 @@ let props = {
 	},
 	user: {
 		error: false,
-		errorMessage: ''
+		errorMessage: '',
+    user: user,
+    following: [],
+    followers: [],
+    projects: [],
+    experience: []
 	},
 	getOneUser: ()=>{},
-	searchUsers: ()=>{},
-	userSearchError: ()=>{}
+  searchUsers: ()=>{},
+  userSearchError: ()=>{},
+  getUserProjects: ()=>{},
+  getUserExperience: ()=>{},
+  getUserFollowing: ()=>{},
+  getUserFollowers: ()=>{}
 }
 describe('UserPage Component', function () {
   it('Component should be built without errors', function () {
-    const wrapper = shallow(<UserPage {...props} user={user}/>);
+    const wrapper = shallow(<UserPage user={props.user}/>);
     expect(wrapper).to.be.ok;
   });
 
   it('should have a CardText Component', function () {
-    const wrapper = shallow(<UserPage {...props} user={user} />);
+    const wrapper = shallow(<UserPage user={props.user} />);
     expect(wrapper.find('CardText').length).to.equal(1);
   });
 
   it('should have a CardHeader component', function () {
-    const wrapper = shallow(<UserPage {...props} user={user} />);
+    const wrapper = shallow(<UserPage user={props.user} />);
     expect(wrapper.find('CardHeader').length).to.equal(1);
   });
 
   it('should have proper values within CardHeader', function () {
-    const wrapper = shallow(<UserPage {...props} user={user} />);
+    const wrapper = shallow(<UserPage user={props.user} />);
     console.log(wrapper.find('CardHeader').props().title)
     const CardHeader = wrapper.find('CardHeader').props();
     expect(CardHeader.title).to.equal('@dave');
@@ -60,16 +69,18 @@ describe('UserPage Component', function () {
   });
 
   it('should have a Card component', function () {
-    const wrapper = shallow(<UserPage {...props} user={user} />);
+    const wrapper = shallow(<UserPage user={props.user} />);
     expect(wrapper.find('Card').length).to.equal(1);
   });
 
   it('should have all props', function () {
-    const wrapper = shallow(<UserPage {...props} user={user} />);
-    expect(wrapper.instance().props.search).to.exist;
+    const wrapper = shallow(<UserPage user={props.user} />);
+
     expect(wrapper.instance().props.user).to.exist;
-    expect(wrapper.instance().props.getOneUser).to.exist;
-    expect(wrapper.instance().props.searchUsers).to.exist;
-    expect(wrapper.instance().props.userSearchError).to.exist;
+    expect(wrapper.instance().props.user.user).to.exist;
+    expect(wrapper.instance().props.user.experience).to.exist;
+    expect(wrapper.instance().props.user.followers).to.exist;
+    expect(wrapper.instance().props.user.following).to.exist;
+    expect(wrapper.instance().props.user.projects).to.exist;
   });
 });
